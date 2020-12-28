a6cf082b73586112179344fe594e3d507677ecd3
Liz-mp <64154575+Liz-mp@users.noreply.github.com>
2020-12-27 20:00:42 -0600

    pie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,7 @@
 @section('content')
     <div id="body_home">     
         <div id="contenido">
-            <br> <br> <br>
-             hola, la verdad aqui ni sé que va,  luego lo hablamos! 
-             <br> <br> <br> <br> <br> <br> <br>
+        
         </div>  
         <div id="pie">
             <div class="top"> aqui va algo</div>
@@ -28,24 +26,7 @@
                         <div class="label">fudebiol@gmail.com</div>
                     </span>
                 </div>
-                <div class="right">
-                    <form class="message-form" action="" method="post">
-                        <div class="title">Dejar un mensaje</div>
-                        <textarea class="body" name="message" placeholder="Mensaje" required></textarea>
-                        <div class="email field">
-                            <label for="message-email">Correo:</label>
-                            <input id="message-email" class="email" name="email" type="email" placeholder="Correo electrónico">
-                        </div>
-                        <div class="Teléfono field">
-                            <label for="message-phone">Teléfono:</label>
-                            <input id="message-phone" class="phone" name="phone" type="text" placeholder="Número de teléfono">
-                        </div>
-                        <input id="message-remember" class="remember" name="remember" type="checkbox">
-                        <label for="message-remember" class="remember-label">Recordar</label>
-                        <input class="submit" type="submit" value="Envíar">
-                    </form>
-                    <div class="authors"><div class="author">Lizeth Monge, Diego Tames, Stefanny Barrantes</div> </div>
-                </div>
+               
             </div>
         </div>
     </div>
