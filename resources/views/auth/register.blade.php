2b03f71e81087353a4ed0ab7bb0e311edb78c745
Stefanny Barrantes <stefanny.barrantes99@gmail.com>
2021-01-04 21:20:34 -0600

    changes in user table, creathe user method

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,11 +14,9 @@
                         @csrf
 
                         <div id="nombre" class="form-group row">
-                            
                             <div  class="col-md-6">
                                  <label><i class="fas fa-user"></i></label>
-                                <input id="name" placeholder="Nombre" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+                                <input id="name" placeholder="Nombre completo" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -27,9 +25,29 @@
                             </div>
                         </div>
 
-                        <div id="correo" class="form-group row contenido">
-                           
+                        <div id="nombre_usuario" class="form-group row">
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                                @error('userame')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
+                        <div id="role" class="form-group row">
+                            <div class="col-md-6">
+                                <input id="role" type="text" class="form-control @error('role') is-invalid @enderror" name="role" value="{{ old('role') }}" required autocomplete="role" autofocus>
+                                @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div id="correo" class="form-group row contenido">
                             <div class="col-md-6">
                                <label><i class="fas fa-envelope"></i></label>
                                 <input id="email" placeholder="E-mail" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
