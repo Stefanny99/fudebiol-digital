f8e556b8e8dbef47aabb610af9c8ea4b9432e827
Liz-mp <64154575+Liz-mp@users.noreply.github.com>
2021-01-05 18:43:28 -0600

    NUEVA INTERFAZ DE USUARIOS

--- resources/views/app/MantenimientoUsuarios.blade.php
+++ resources/views/app/MantenimientoUsuarios.blade.php
@@ -41,35 +41,32 @@
                   <th>Rol</th>
                 </tr>
                 <tr>
-                  <th>1</th>
-                  <td>Diego Tames Vargas</td>
-                  <td>thamesdiego@gmail.com</td>
-                  <td>Tames</td>
-              
-                  <td>Sitio web</td>
+                  <td class="number">1</td>
+                  <td class="fila">Diego Tames Vargas</td>
+                  <td class="fila">thamesdiego@gmail.com</td>
+                  <td class="fila">Tames</td>
+                  <td class="fila">Sitio web</td>
                 </tr>
                 <tr>
-                  <th>2</th>
-                 <td>Lizeth Monge Padilla</td>
-                 <td>lizmonge15@gmail.com</td>
-                 <td>Liz MP</td>
-              
-                 <td>Mi árbol para la vida</td>
+                  <td  class="number">2</td>
+                 <td class="fila">Lizeth Monge Padilla</td>
+                 <td class="fila">lizmonge15@gmail.com</td>
+                 <td class="fila">Liz MP</td>
+                 <td class="fila">Mi árbol para la vida</td>
                 </tr>
                  <tr>
-                  <th>3</th>
-                 <td>Stefanny Barrantes Vargas</td>
-                 <td>barrantesdenia@gmail.com</td>
-                 <td>Tefa</td>
-              
-                 <td>Sitio web</td>
+                  <td class="number">3</td>
+                 <td class="fila">Stefanny Barrantes Vargas</td>
+                 <td class="fila">barrantesdenia@gmail.com</td>
+                 <td class="fila">Tefa</td>
+                 <td class="fila">Sitio web</td>
                 </tr>
               </tbody>
             </table>
             <div id="botonesEdicion">
                 <button class="btn_guardar"> Guardar</button>
-                <button class="btn_eliminar"> Eliminar</button>
-                <button class="btn_editar"> Editar</button>
+                <button class="btn_guardar"> Eliminar</button>
+                <button class="btn_guardar"> Editar</button>
             </div>
           </div>
         </div>
