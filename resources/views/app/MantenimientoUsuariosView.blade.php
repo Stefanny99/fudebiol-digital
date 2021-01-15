e2b7cc4042954462928f696959b0f124b7931960
Liz-mp <64154575+Liz-mp@users.noreply.github.com>
2021-01-15 13:05:55 -0600

    cambios de interfaz

--- resources/views/app/MantenimientoUsuariosView.blade.php
+++ resources/views/app/MantenimientoUsuariosView.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div id="body_home">     
         <div id="contenido">
+          <div id="fondo1">
           <input type="checkbox" id="check" checked="checked">
           <form id="agregarUsuario" method="post" action="{{ route( 'insertarUsuario' ) }}">
             @csrf
@@ -13,15 +14,15 @@
                <i class="fas fa-user-plus" id=""></i>
             </label>
             <label id="RegUsuarios">Registrar Usuario</label>
-            <label class="texto" for="name">Nombre completo</label>
+            <label class="texto" for="name">Nombre completo:</label>
             <input type="text" name="name">
-            <label class="texto" for="email">E-mail</label>
+            <label class="texto" for="email">E-mail:</label>
             <input type="text" name="email">
-            <label class="texto" for="username">Usuario</label>
+            <label class="texto" for="username">Usuario:</label>
             <input type="text" name="username">
-            <label class="texto" for="password">Contraseña</label>
+            <label class="texto" for="password">Contraseña:</label>
             <input type="password" name="password">
-            <label class="texto" for="password_confirmation">Confirmar Contraseña</label>
+            <label class="texto" for="password_confirmation">Confirmar Contraseña:</label>
             <input type="password" name="password_confirmation">
             <label class="texto" for="role">Rol</label>
             <select name="role">
@@ -36,18 +37,18 @@
         <table id="tablaUsuarios" >
               <caption>Administradores Fudebiol Digital</caption>
               <tbody>
-                <tr>
-                  <th>Nombre</th>
-                  <th>E-mail</th>
-                  <th>Usuario</th>
-                  <th>Rol</th>
+                <tr class="thead">
+                  <th class="thead" >Nombre</th>
+                  <th class="thead">E-mail</th>
+                  <th class="thead">Usuario</th>
+                  <th class="thead">Rol</th>
                 </tr>
                 @foreach ( $usuarios as $usuario )
                 <tr class="fila">
-                  <td class="fila">{{ $usuario->name }}</td>
-                  <td class="fila">{{ $usuario->email }}</td>
-                  <td class="fila">{{ $usuario->username }}</td>
-                  <td class="fila">{{ $usuario->role }}</td>
+                  <td >{{ $usuario->name }}</td>
+                  <td >{{ $usuario->email }}</td>
+                  <td >{{ $usuario->username }}</td>
+                  <td >{{ $usuario->role }}</td>
                 </tr>
                 @endforeach
               </tbody>
@@ -58,6 +59,7 @@
                 <button class="btn_guardar"> Editar</button>
             </div>
           </div>
+        </div>
         </div>
     </div>
 @endsection
