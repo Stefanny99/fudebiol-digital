96431caab06c2c8b25aeec3db410ca71f1c9a24b
Liz-mp <64154575+Liz-mp@users.noreply.github.com>
2021-01-25 16:46:29 -0600

    pasar imagenes

--- public/js/galeria.js
+++ public/js/galeria.js
@@ -1,4 +1,4 @@
-
+var f;
 function mostrarFoto(foto){
 	var base= document.body;
 	var contenedor= document.createElement("div");
@@ -6,22 +6,23 @@ function mostrarFoto(foto){
 	var contenedor2= document.createElement("div");
 	contenedor2.id="contenedor2";
 	var imagen=document.createElement("img");
-	imagen.className="foto";
+	imagen.id="foto";
 	imagen.src=foto.src;
 	var descripcion=document.createElement("div");
-	descripcion.className="descripcion";
-	descripcion.innerHTML="Luego, continúa la historia con un recuerdo de niñez del aviador: una imagen de un elefante dentro de una boa en la que las personas mayores sólo veían un sombrero en vez de lo que realmente era. Pero cuando el aviador sufre una avería en el desierto del Sáhara y se encuentra con un hombrecito –el pequeño príncipe-, éste acierta ver en el dibujo “la boa cerrada”. De esta forma la historia del aviador francés consigue plasmar la gran brecha que separa el mundo infantil y el mundo adulto, “el primero, regido por la fantasía, y el segundo, basado en la lógica”, tal como destaca la psicóloga y psicoterapeuta infanto-juvenil y familiar Amalia Gordóvil Merino. Según la psicoterapeuta, con este relato sencillo, profundo y conmovedor, el autor habla de “la necesidad de entrar en el mundo de los más pequeños escuchándolos y disfrutando con ellos'";
+	descripcion.id="descripcion";
+	descripcion.innerHTML=foto.getAttribute("data-descripcion");
 	var botones= document.createElement("div");
 	botones.id="botones";
 	var next=document.createElement("label");
 	next.classList.add("next");
 	next.classList.add("hvr-backward");
 	next.innerHTML="<i class='fas fa-chevron-right'></i>";
-	next.onclick=nextImage(foto);
+	next.onclick=nextImage;
 	var prev=document.createElement("label");
 	prev.classList.add("prev");
 	prev.classList.add("hvr-forward");
 	prev.innerHTML='<i class="fas fa-chevron-left"></i>';
+	prev.onclick=prevImage;
 	var exit=document.createElement("label");
 	exit.classList.add("exit");
 	exit.onclick=remover;
@@ -38,10 +39,41 @@ panel.remove();
 }
 
 
-function nextImage(foto){
+function nextImage(){
+var contImg=document.getElementById("foto");
+var contDesc=document.getElementById("descripcion");
+var imagen=contImg.src;
+var imagenes=document.getElementById("row").getElementsByTagName("img");
+var ImagenesLista = Array.prototype.slice.call(imagenes);
+            	for(let i = 0; i < imagenes.length; i++)
+				{
+				    if(imagen == imagenes[i].src && i < imagenes.length-1 ){
+				    	console.log(imagenes[i+1].src);
+				    	 contImg.src=imagenes[i+1].src;
+				    	 contDesc.innerHTML=imagenes[i+1].getAttribute("data-descripcion");
+				    	 break;
+				    }
 
-var imagen=foto;
-var siguiente=document.getElementById('cajafoto').getElementsByTagName('img')[0];
-console.log(siguiente.src);
+				}
+
+}
+
+function prevImage(){
+var contImg=document.getElementById("foto");
+var contDesc=document.getElementById("descripcion");
+var imagen=contImg.src;
+var imagenes=document.getElementById("row").getElementsByTagName("img");
+var ImagenesLista = Array.prototype.slice.call(imagenes);
+            	for(let i = 0; i < imagenes.length; i++)
+				{
+				    if(imagen == imagenes[i].src && i >0){
+				    	console.log(imagenes[i-1].src);
+				    	 contImg.src=imagenes[i-1].src;
+				    	 contDesc.innerHTML=imagenes[i-1].getAttribute("data-descripcion");
+				    	 break;
+
+				    }
+
+				}
 
 }
--- resources/views/app/GaleriaView.blade.php
+++ resources/views/app/GaleriaView.blade.php
@@ -10,34 +10,34 @@
          </div>
         <div class="home"></div>
                
-                <div class="row">
-                     <div class="inner" id="cajafoto"><img src="/img/large/gm1.jpg" alt="image"  onclick="mostrarFoto(this);" /></div>
+                <div id="row">
+                     <div class="inner"  ><img  src="/img/large/gm1.jpg" alt="image"  data-descripcion="hola bubu0" onclick="mostrarFoto(this);" /></div>
                         <div class="inner">
-                           <img src="/img/large/gm2.jpg" alt="image" onclick="mostrarFoto(this);" />
+                           <img src="/img/large/gm2.jpg" alt="image" data-descripcion="hola bubu1" onclick="mostrarFoto(this);" />
                        </div>
                    
-                        <div class="inner"><img src="/img/large/gm3.jpg" alt="image" onclick="mostrarFoto(this);" /></div>
+                        <div class="inner"><img src="/img/large/gm3.jpg" data-descripcion="hola bubu2" alt="image" onclick="mostrarFoto(this);" /></div>
                    
                         <div class="inner">
-                            <img src="/img/large/gm4.jpg" alt="image"  onclick="mostrarFoto(this);"/>
+                            <img src="/img/large/gm4.jpg" alt="image" data-descripcion="hola bubu3"  onclick="mostrarFoto(this);"/>
                       
                         </div>
                  
                   
                         <div class="inner">
-                          <img src="/img/large/gm6.jpg" alt="image" onclick="mostrarFoto(this);" /> 
+                          <img src="/img/large/gm5.jpg"  data-descripcion="hola bubu4" alt="image" onclick="mostrarFoto(this);" /> 
                         </div>
                         <div class="inner">
-                           <img src="/img/large/gm1.jpg" alt="image" onclick="mostrarFoto(this);"/>
+                           <img src="/img/large/gm6.jpg" alt="image" data-descripcion="hola bubu5" onclick="mostrarFoto(this);"/>
                       </div>
                    
                         <div class="inner">
-                           <img src="/img/large/gm2.jpg" alt="image" onclick="mostrarFoto(this);"/>
+                           <img src="/img/large/gm7.jpg" alt="image"  data-descripcion="hola bubu6" onclick="mostrarFoto(this);"/>
                        </div>
                    
                      <div class="inner">
                            
-                          <img src="/img/large/gm3.jpg" alt="image" onclick="mostrarFoto(this);" />
+                          <img src="/img/large/gm8.jpg" alt="image" data-descripcion="hola bubu7" onclick="mostrarFoto(this);" />
                            
                         </div>
                        
