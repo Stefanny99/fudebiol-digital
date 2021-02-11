2a0e5c5d07837d3614bfb3c95da10454dd15a980
Liz-mp <64154575+Liz-mp@users.noreply.github.com>
2021-02-11 10:25:29 -0600

    Add existing file

--- resources/views/app/GaleriaView.blade.php
+++ resources/views/app/GaleriaView.blade.php
@@ -3,74 +3,58 @@
 @section('content')
 
     <div id="body_home"> 
-      <div id="contenido">
-         <div id="fondoGaleria">
-          <div id="tituloGaleria" class="container-fluid">
-            <label>Galería</label>
-          </div>
-         </div>
-        <div class="home">
-               
+        <div id="contenido">
+            <div id="fondoGaleria" >
+                <div class="titulo tituloGaleria">
+                    <img id="logoMAPLV" src="{{ asset('/img/logo.jpg')}}">
+                    <label >Galería</label>
+                    <label  id="subText">¡Explora FUDEBIOL!</label>
+                    </div>
+                </div>
+            </div>
+            <div class="home">
                 <div id="row">
-                     <div class="inner"  ><img class="img-responsive" src="/img/fude1.jpg" alt="image"  data-descripcion="Fudebiol te ofrece un espectacular lago donde puedes navegar y estar en paz." onclick="mostrarFoto(this);" /></div>
-                        <div class="inner">
-                           <img class="img-responsive" src="/img/fude2.jpg" alt="image" data-descripcion="Contamos con distintos espacios para disfrutar en familia." onclick="mostrarFoto(this);" />
-                       </div>
-                   
-                        <div class="inner"><img class="img-responsive"  src="/img/fude3.jpg" data-descripcion="Puedes navegar en nuestro lago, puedes observar la distinta vegetación dentro de ella y los peces." alt="image" onclick="mostrarFoto(this);" /></div>
-                   
-                        <div class="inner">
-                            <img src="/img/fude4.jpg" class="img-responsive"  alt="image" data-descripcion="Un ambiente de paz..."  onclick="mostrarFoto(this);"/>
-                      
-                        </div>
-                 
-                  
-                        <div class="inner">
-                          <img src="/img/fude6.jpg" class="img-responsive"  data-descripcion="Siéntete de la mano con la naturaleza" alt="image" onclick="mostrarFoto(this);" /> 
-                        </div>
-                        <div class="inner">
-                           <img src="/img/fude_banner.jpg" class="img-responsive"  alt="image" data-descripcion="Ún sitio siempre verde.." onclick="mostrarFoto(this);"/>
-                      </div>
-                   
-                        <div class="inner">
-                           <img src="/img/fude7.jpg" class="img-responsive"  alt="image"  data-descripcion="Hermosas esculturas..." onclick="mostrarFoto(this);"/>
-                       </div>
-                   
-                     <div class="inner">
-                           
-                          <img src="/img/fude9.jpg" class="img-responsive"  alt="image" data-descripcion="Disfrutra de la naturaleza con nosotros." onclick="mostrarFoto(this);" />
-                           
-                        </div>
-                       
-                  
+                    <div class="inner"  >
+                        <img class="img-responsive" src="/img/fude1.jpg" alt="image"  data-descripcion="Fudebiol te ofrece un espectacular lago donde puedes navegar y estar en paz." onclick="mostrarFoto(this);" />
+                    </div>
+                    <div class="inner">
+                        <img class="img-responsive" src="/img/fude2.jpg" alt="image" data-descripcion="Contamos con distintos espacios para disfrutar en familia." onclick="mostrarFoto(this);" />
+                    </div>
+                    
+                    <div class="inner">
+                        <img class="img-responsive"  src="/img/fude3.jpg" data-descripcion="Puedes navegar en nuestro lago, puedes observar la distinta vegetación dentro de ella y los peces." alt="image" onclick="mostrarFoto(this);" />
+                    </div>
+                    
+                    <div class="inner">
+                        <img src="/img/fude4.jpg" class="img-responsive"  alt="image" data-descripcion="Un ambiente de paz..."  onclick="mostrarFoto(this);"/>
+                        
+                    </div>
+                    
+                    
+                    <div class="inner">
+                        <img src="/img/fude6.jpg" class="img-responsive"  data-descripcion="Siéntete de la mano con la naturaleza" alt="image" onclick="mostrarFoto(this);" /> 
+                    </div>
+                    <div class="inner">
+                        <img src="/img/fude_banner.jpg" class="img-responsive"  alt="image" data-descripcion="Ún sitio siempre verde.." onclick="mostrarFoto(this);"/>
+                    </div>
+                    
+                    <div class="inner">
+                        <img src="/img/fude7.jpg" class="img-responsive"  alt="image"  data-descripcion="Hermosas esculturas..." onclick="mostrarFoto(this);"/>
+                    </div>
+                    
+                    <div class="inner">
+                            
+                        <img src="/img/fude9.jpg" class="img-responsive"  alt="image" data-descripcion="Disfrutra de la naturaleza con nosotros." onclick="mostrarFoto(this);" />
+                            
+                    </div>
+                        
+                    
                 </div>
             
-   </div>
-      
-      </div>
-      </div>
-       <div id="pie">
-            <div class="bottom">
-                <div class="left">
-                    <img class="icon img-responsive" src="img/vector.png"></img>
-                    <div class="sitename">&copy;FUDEBIOL</div>
-                </div>
-                <div class="middle">
-                    <a class="facebook contact" href="https://www.facebook.com/FUDEBIOL/">
-                        <img class="icon img-responsive" src="img/facebook.png"></img>
-                        <div class="label">@FUDEBIOL</div>
-                    </a>
-                    <a class="whatsapp contact" href="">
-                        <img class="icon img-responsive" src="img/whatsapp.png"></img>
-                        <div class="label">7265-9372</div>
-                    </a>
-                    <span class="email contact">
-                        <img class="icon img-responsive" src="img/email.png"></img>
-                        <div class="label">fudebiol@gmail.com</div>
-                    </span>
-                </div>
-               
             </div>
+      
         </div>
     </div>
+      
+        
 @endsection
---
+++ resources/views/app/aboutUsView.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="body_home">     
+        <div id="contenido">
+            <div id="fondoAboutUs" >
+                <div class="tituloAU">
+                    <img  src="img/logo.jpg">
+                    <label >¿Quiénes somos?</label>
+                    </div>
+                </div>
+            </div>
+            <div class="home">
+                <h1>Bienvenido a FUDEBIOL</h1>
+                <p>La Fundación para el Desarrollo del Centro Biológico las Quebradas (FUDEBIOL)
+                es una organización de carácter social, sin fines de lucro, constituida el 13 de
+                mayo de 1989, bajo el marco legal de la Ley de Fundaciones No. 5338 en Costa Rica;
+                con el propósito de proteger y conservar los recursos naturales de la Cuenca 
+                Superior del Río Quebradas, por el potencial que representa el Bosque Nuboso 
+                Tropical Premontano Húmedo de la Cordillera de Talamanca - Sector Pacífico, así 
+                como el área de recarga acuífera del acueducto de agua potable que abastece a 
+                100.000 habitantes en la Ciudad de San Isidro de El General.
+                </p>
+               
+   
+                <h1>Nuestros objetivos</h1>
+                <p>
+                Los objetivos de FUDEBIOL están relacionados con la protección del Río 
+                Quebradas, mediante la conservación de la biodiversidad, el impulso de
+                 actividades de desarrollo sostenible como la agricultura conservacionista, 
+                 el ecoturismo y el desarrollo científico; que facilite elevar el nivel de 
+                 vida de sus pobladores, en apoyo al proceso de desarrollo de la Región del 
+                 Pacífico Sur de Costa Rica.
+                </p>
+
+            </div> <!-- FIN DE HOME-->
+        </div>
+    </div>
+@endsection
