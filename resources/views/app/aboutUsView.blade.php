795ca0b02422ef3c3b427936f9179fc242be4437
Liz-mp <64154575+Liz-mp@users.noreply.github.com>
2021-02-13 10:25:02 -0600

    info

--- resources/views/app/aboutUsView.blade.php
+++ resources/views/app/aboutUsView.blade.php
@@ -11,7 +11,21 @@
                 </div>
             </div>
             <div class="home">
-                <h1>Bienvenido a FUDEBIOL</h1>
+                
+               
+   
+               
+               
+<div class="carrusel">
+<section class="carousel" aria-label="Gallery">
+  <ol class="carousel__viewport">
+    <li id="carousel__slide1"
+        tabindex="0"
+        class="carousel__slide">
+      <div class="carousel__snapper">
+      <img class="fotocarrusel" src="{{ asset('/img/av1.png')}}">
+         <div>
+         <h1>Bienvenido a FUDEBIOL</h1>
                 <p>La Fundación para el Desarrollo del Centro Biológico las Quebradas (FUDEBIOL)
                 es una organización de carácter social, sin fines de lucro, constituida el 13 de
                 mayo de 1989, bajo el marco legal de la Ley de Fundaciones No. 5338 en Costa Rica;
@@ -21,9 +35,21 @@
                 como el área de recarga acuífera del acueducto de agua potable que abastece a 
                 100.000 habitantes en la Ciudad de San Isidro de El General.
                 </p>
-               
-   
-                <h1>Nuestros objetivos</h1>
+         </div>
+      </div>
+        <a href="#carousel__slide4"
+           class="carousel__prev">Go to last slide</a>
+        <a href="#carousel__slide2"
+           class="carousel__next">Go to next slide</a>
+     
+    </li>
+    <li id="carousel__slide2"
+        tabindex="0"
+        class="carousel__slide">
+      <div class="carousel__snapper">
+         <img class="fotocarrusel" src="{{ asset('/img/av2.jpg')}}">
+         <div>
+         <h1>Nuestros objetivos</h1>
                 <p>
                 Los objetivos de FUDEBIOL están relacionados con la protección del Río 
                 Quebradas, mediante la conservación de la biodiversidad, el impulso de
@@ -32,8 +58,74 @@
                  vida de sus pobladores, en apoyo al proceso de desarrollo de la Región del 
                  Pacífico Sur de Costa Rica.
                 </p>
-
-            </div> <!-- FIN DE HOME-->
+         </div>
+        </div>
+      <a href="#carousel__slide1"
+         class="carousel__prev">Go to previous slide</a>
+      <a href="#carousel__slide3"
+         class="carousel__next">Go to next slide</a>
+    </li>
+    <!-- <li id="carousel__slide3"
+        tabindex="0"
+        class="carousel__slide">
+      <div class="carousel__snapper">
+      <img class="fotocarrusel" src="{{ asset('/img/b11.jpg')}}">
+         <div>Hola jdfhgsjd fsdgf hjgsdjhf gsdg fsdgffsdhjfg sdhjfg
+             sdgvfhsdgfhjgsdf sdf 
+             ds get_defined_constantsdfg dfg dfg
+         </div>
+      </div>
+      <a href="#carousel__slide2"
+         class="carousel__prev">Go to previous slide</a>
+      <a href="#carousel__slide4"
+         class="carousel__next">Go to next slide</a>
+    </li> -->
+    <!-- <li id="carousel__slide4"
+        tabindex="0"
+        class="carousel__slide">
+      <div class="carousel__snapper">
+      <img class="fotocarrusel" src="{{ asset('/img/b11.jpg')}}">
+         <div>Hola VAMOS A VER SI ESTO SE MUESTRA
+            asdfhakjsdhajsd adsf 
+            afa forsd getAuthenticatorsForAgentag dfg 
+            dafg 
+            adfg adf g
+            dfg adadfg addgadf get_defined_constantsdfgfag adfga 
+            f 
+         </div>
+      </div>
+      <a href="#carousel__slide3"
+         class="carousel__prev">Go to previous slide</a>
+      <a href="#carousel__slide1"
+         class="carousel__next">Go to first slide</a>
+    </li> -->
+  </ol>
+  <aside class="carousel__navigation">
+    <ol class="carousel__navigation-list">
+      <li class="carousel__navigation-item">
+        <a href="#carousel__slide1"
+           class="carousel__navigation-button">Go to slide 1</a>
+      </li>
+      <li class="carousel__navigation-item">
+        <a href="#carousel__slide2"
+           class="carousel__navigation-button">Go to slide 2</a>
+      </li>
+      <!-- <li class="carousel__navigation-item">
+        <a href="#carousel__slide3"
+           class="carousel__navigation-button">Go to slide 3</a>
+      </li>
+      <li class="carousel__navigation-item">
+        <a href="#carousel__slide4"
+           class="carousel__navigation-button">Go to slide 4
+           </a>
+           
+      </li> -->
+    </ol>
+  </aside>
+</section>
+</div> <!-- carrusel --> 
+</div> <!-- FIN DE HOME-->
+            
         </div>
     </div>
 @endsection
