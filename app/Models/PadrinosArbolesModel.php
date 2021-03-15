c7583f248da9b4253b1f5efa48dfcace0dd951d9
Stefanny Barrantes <stefanny.barrantes99@gmail.com>
2021-03-15 17:08:18 -0600

    validación

--- app/Models/PadrinosArbolesModel.php
+++ app/Models/PadrinosArbolesModel.php
@@ -31,18 +31,22 @@ class PadrinosArbolesModel extends Model {
             "accion" => "PadrinosArbolesModel:eliminarPadrinoArbol"
         );
         try {
-            $imagen = DB::table( "fudebiol_imagenes" )->where( 'fi_id', $request->input( "fi_id" ) )->get();
+            $padrino_arbol = DB::table( "fudebiol_padrinos_arboles" )->where( 'fpa_id', $request->input( "fpa_id" ) )->get();
             DB::begintransaction();
             try {
-                DB::table('fudebiol_padrinos_arboles')->where('fpa_id', $request->input('fpa'))->delete();
-                try {
-                    Storage::delete( "public/img/fudebiol_adopciones/" . $imagen->FA_ID . $imagen->FA_IMAGEN_FORMATO );
-                    DB::commit(); 
-                } catch(Exception $e){
-                    $data['codigo'] = Util::$codigos[ "ERROR_ELIMINANDO" ];
-                    $data['razon'] = "Ocurrió un error al eliminar la imagen del árbol";
-                    DB::rollBack();
-                }   
+                DB::table('fudebiol_padrinos_arboles')->where('fpa_id', $request->input('fpa_id'))->delete();
+                if ($padrino_arbol->FPA_IMAGEN_FORMATO){
+                    try {
+                        Storage::delete( "public/img/fudebiol_adopciones/" . $padrino_arbol->FPA_ID . $padrino_arbol->FPA_IMAGEN_FORMATO );
+                        DB::commit(); 
+                    } catch(Exception $e){
+                        $data['codigo'] = Util::$codigos[ "ERROR_ELIMINANDO" ];
+                        $data['razon'] = "Ocurrió un error al eliminar el comprobante de la adopción";
+                        DB::rollBack();
+                    }  
+                }else {
+                    DB::commit();
+                }  
             } catch (Exception $e) {
                 $data['codigo'] = Util::$codigos[ "ERROR_ELIMINANDO" ];
                 $data['razon'] = "Ocurrió un error al eliminar la adopción";
@@ -51,12 +55,6 @@ class PadrinosArbolesModel extends Model {
         }catch(Exception $e){
             $data[ 'codigo' ] = Util::$codigos[ "ERROR_DE_SERVIDOR" ];
             Log::error( $e->getMessage(), $data );
-        }
-
-        try{
-            DB::table('fudebiol_padrinos_arboles')->where('fpa_id', $request->input('fpa_id'))->delete();
-        }catch(Exception $e){
-            $data[ 'codigo' ] =  Util::$codigos[ "ERROR_ELIMINANDO" ];
         }
         return $data;
     }
