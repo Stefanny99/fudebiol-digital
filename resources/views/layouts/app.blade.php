913c3bdcb6a7ee43c7d0d8a0f1a075d8d31f545e
thamesdiego@gmail.com <4A45b105C160d13E95f>
2021-03-26 18:50:50 -0600

    Actualización Publicaciones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/alertify.min.js') }}"></script>
+    <script src="{{ asset('js/axios.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/galeria.js') }}"></script>
     <script src="{{ asset('js/mantenimiento-usuarios.js') }}"></script>
