f522e46e4e1aaf7a6ef53193c469c65678c69be9
thamesdiego@gmail.com <4A45b105C160d13E95f>
2021-03-31 19:57:58 -0600

    Update GaleriaView.blade.php

--- resources/views/app/GaleriaView.blade.php
+++ resources/views/app/GaleriaView.blade.php
@@ -1,15 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <?php
-        function background_mask( $url ){
-            return "-o-mask: url( '$url' ) center/cover;"
-            . "-ms-mask: url( '$url' ) center/cover;"
-            . "-moz-mask: url( '$url' ) center/cover;"
-            . "-webkit-mask: url( '$url' ) center/cover;"
-            . "mask: url( '$url' ) center/cover;";
-        }
-    ?>
+
     <div id="body_home"> 
         <div id="contenido">
             <div id="fondoGaleria" >
@@ -23,7 +15,7 @@
             <div class="home">
                 <div id="row">
                     @foreach ( $imagenes as $imagen )
-                    <div class="inner" style="{{ background_mask( asset( 'storage/img/fudebiol_imagenes/' . $imagen->FI_ID . '.' . $imagen->FI_FORMATO ) ) }}">
+                    <div class="inner">
                         <img class="img-responsive" src="{{ asset( 'storage/img/fudebiol_imagenes/' . $imagen->FI_ID . '.' . $imagen->FI_FORMATO ) }}" alt="image"  data-descripcion="{{ $imagen->FI_DESCRIPCION }}" onclick="mostrarFoto(this);" />
                     </div>
                     @endforeach
