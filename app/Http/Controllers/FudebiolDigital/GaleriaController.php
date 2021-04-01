ab1e87ffb9e63e05a75adfe58ce3ffd296577731
thamesdiego@gmail.com <4A45b105C160d13E95f>
2021-03-31 18:17:33 -0600

    Galería
    - Cargar imágenes de galería

--- app/Http/Controllers/FudebiolDigital/GaleriaController.php
+++ app/Http/Controllers/FudebiolDigital/GaleriaController.php
@@ -1,87 +1,94 @@
 <?php
-	namespace App\Http\Controllers\FudebiolDigital;
+    namespace App\Http\Controllers\FudebiolDigital;
 
-	use App\Http\Controllers\Controller;
-	use Illuminate\Http\Request;
-	use Illuminate\Support\Facades\Validator;
-	use Session;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Validator;
+    use Session;
 
-	use App\Helper\Util;
-	use App\Models\GaleriaModel;
+    use App\Helper\Util;
+    use App\Models\GaleriaModel;
 
-	class GaleriaController extends Controller{
-		public function galeria(){
-			return view('app\GaleriaView');
-		}
+    class GaleriaController extends Controller{
+        public function galeria(){
+            $model = new GaleriaModel();
+            $result = $model->obtenerImagenes();
+            if ( $result[ "codigo" ][ "codigo" ] != Util::$codigos[ "EXITO" ][ "codigo" ] ){
+                return redirect()->back()->with( "errores", array( $result[ "codigo" ][ "descripcion" ] . ", " . $result[ "razon" ] ) )->withInput( $request->input() );
+            }
+            return view( 'app\GaleriaView', array(
+                "imagenes" => $result[ "resultado" ]
+            ) );
+        }
 
-		public function editarGaleria(){
-			$model = new GaleriaModel();
-			$result = $model->obtenerImagenes();
-			if ( $result[ "codigo" ][ "codigo" ] != Util::$codigos[ "EXITO" ][ "codigo" ] ){
-				return redirect()->back()->with( "errores", array( $result[ "codigo" ][ "descripcion" ] . ", " . $result[ "razon" ] ) )->withInput( $request->input() );
-			}
-			return view('app\EditarGaleriaView', array(
-				"imagenes" => $result[ "resultado" ]
-			) );
-		}
+        public function editarGaleria(){
+            $model = new GaleriaModel();
+            $result = $model->obtenerImagenes();
+            if ( $result[ "codigo" ][ "codigo" ] != Util::$codigos[ "EXITO" ][ "codigo" ] ){
+                return redirect()->back()->with( "errores", array( $result[ "codigo" ][ "descripcion" ] . ", " . $result[ "razon" ] ) )->withInput( $request->input() );
+            }
+            return view('app\EditarGaleriaView', array(
+                "imagenes" => $result[ "resultado" ]
+            ) );
+        }
 
-		public function guardarFotos( Request $request ){
-			$validator = Validator::make( $request->all(), [
-				"fotos" => [ "required", "array" ],
-				"fotos.*" => [ "image" ],
-				"descripciones" => [ "required", "array" ],
-				"descripciones.*" => [ "string", "max:1000" ]
-			] );
-			if ( $validator->fails() ){
-				return redirect()->back()->with( "errores", $validator->errors()->all() )->withInput( $request->input() );
-			}else{
-				$model = new GaleriaModel();
-				$result = $model->agregarImagenes( $request );
-				if ( $result[ "codigo" ][ "codigo" ] != Util::$codigos[ "EXITO" ][ "codigo" ] ){
-					return redirect()->back()->with( "errores", array( $result[ "codigo" ][ "descripcion" ] . ", " . $result[ "razon" ] ) )->withInput( $request->input() );
-				}
-				return redirect()->back()->with( "mensajes", array(
-					"Imágenes guardadas con éxito"
-				) );
-			}
-		}
+        public function guardarFotos( Request $request ){
+            $validator = Validator::make( $request->all(), [
+                "fotos" => [ "required", "array" ],
+                "fotos.*" => [ "image" ],
+                "descripciones" => [ "required", "array" ],
+                "descripciones.*" => [ "string", "max:1000" ]
+            ] );
+            if ( $validator->fails() ){
+                return redirect()->back()->with( "errores", $validator->errors()->all() )->withInput( $request->input() );
+            }else{
+                $model = new GaleriaModel();
+                $result = $model->agregarImagenes( $request );
+                if ( $result[ "codigo" ][ "codigo" ] != Util::$codigos[ "EXITO" ][ "codigo" ] ){
+                    return redirect()->back()->with( "errores", array( $result[ "codigo" ][ "descripcion" ] . ", " . $result[ "razon" ] ) )->withInput( $request->input() );
+                }
+                return redirect()->back()->with( "mensajes", array(
+                    "Imágenes guardadas con éxito"
+                ) );
+            }
+        }
 
-		public function editarFoto( Request $request ){
-			if ( !$request->has( "eliminar" ) ){
-				$validator = Validator::make( $request->all(), [
-					"fi_id" => [ "required", "integer" ],
-					"fi_descripcion" => [ "required", "string", "max:1000" ]
-				] );
-				if ( $validator->fails() ){
-					return redirect()->back()->with( "errores", $validator->errors()->all() )->withInput( $request->input() );
-				}else{
-					$model = new GaleriaModel();
-					$result = $model->editarImagen( $request );
-					if ( $result[ "codigo" ][ "codigo" ] != Util::$codigos[ "EXITO" ][ "codigo" ] ){
-						return redirect()->back()->with( "errores", array( $result[ "codigo" ][ "descripcion" ] . ", " . $result[ "razon" ] ) )->withInput( $request->input() );
-					}
-					return redirect()->back()->with( "mensajes", array(
-						"Imagen actualizada exitosamente"
-					) );
-				}
-			}else{
-				$validator = Validator::make( $request->all(), [
-					"fi_id" => [ "required", "integer" ],
-					"fi_formato" => [ "required", "string", "max:5" ]
-				] );
-				if ( $validator->fails() ){
-					return redirect()->back()->with( "errores", $validator->errors()->all() )->withInput( $request->input() );
-				}else{
-					$model = new GaleriaModel();
-					$result = $model->eliminarImagen( $request );
-					if ( $result[ "codigo" ][ "codigo" ] != Util::$codigos[ "EXITO" ][ "codigo" ] ){
-						return redirect()->back()->with( "errores", array( $result[ "codigo" ][ "descripcion" ] . ", " . $result[ "razon" ] ) )->withInput( $request->input() );
-					}
-					return redirect()->back()->with( "mensajes", array(
-						"Imagen eliminada exitosamente"
-					) );
-				}
-			}
-		}
-	}
+        public function editarFoto( Request $request ){
+            if ( !$request->has( "eliminar" ) ){
+                $validator = Validator::make( $request->all(), [
+                    "fi_id" => [ "required", "integer" ],
+                    "fi_descripcion" => [ "required", "string", "max:1000" ]
+                ] );
+                if ( $validator->fails() ){
+                    return redirect()->back()->with( "errores", $validator->errors()->all() )->withInput( $request->input() );
+                }else{
+                    $model = new GaleriaModel();
+                    $result = $model->editarImagen( $request );
+                    if ( $result[ "codigo" ][ "codigo" ] != Util::$codigos[ "EXITO" ][ "codigo" ] ){
+                        return redirect()->back()->with( "errores", array( $result[ "codigo" ][ "descripcion" ] . ", " . $result[ "razon" ] ) )->withInput( $request->input() );
+                    }
+                    return redirect()->back()->with( "mensajes", array(
+                        "Imagen actualizada exitosamente"
+                    ) );
+                }
+            }else{
+                $validator = Validator::make( $request->all(), [
+                    "fi_id" => [ "required", "integer" ],
+                    "fi_formato" => [ "required", "string", "max:5" ]
+                ] );
+                if ( $validator->fails() ){
+                    return redirect()->back()->with( "errores", $validator->errors()->all() )->withInput( $request->input() );
+                }else{
+                    $model = new GaleriaModel();
+                    $result = $model->eliminarImagen( $request );
+                    if ( $result[ "codigo" ][ "codigo" ] != Util::$codigos[ "EXITO" ][ "codigo" ] ){
+                        return redirect()->back()->with( "errores", array( $result[ "codigo" ][ "descripcion" ] . ", " . $result[ "razon" ] ) )->withInput( $request->input() );
+                    }
+                    return redirect()->back()->with( "mensajes", array(
+                        "Imagen eliminada exitosamente"
+                    ) );
+                }
+            }
+        }
+    }
 ?>
--- resources/views/app/GaleriaView.blade.php
+++ resources/views/app/GaleriaView.blade.php
@@ -14,41 +14,11 @@
             </div>
             <div class="home">
                 <div id="row">
+                    @foreach ( $imagenes as $imagen )
                     <div class="inner"  >
-                        <img class="img-responsive" src="/img/fude1.jpg" alt="image"  data-descripcion="Fudebiol te ofrece un espectacular lago donde puedes navegar y estar en paz." onclick="mostrarFoto(this);" />
+                        <img class="img-responsive" src="{{ asset( 'storage/img/fudebiol_imagenes/' . $imagen->FI_ID . '.' . $imagen->FI_FORMATO ) }}" alt="image"  data-descripcion="{{ $imagen->FI_DESCRIPCION }}" onclick="mostrarFoto(this);" />
                     </div>
-                    <div class="inner">
-                        <img class="img-responsive" src="/img/fude2.jpg" alt="image" data-descripcion="Contamos con distintos espacios para disfrutar en familia." onclick="mostrarFoto(this);" />
-                    </div>
-                    
-                    <div class="inner">
-                        <img class="img-responsive"  src="/img/fude3.jpg" data-descripcion="Puedes navegar en nuestro lago, puedes observar la distinta vegetación dentro de ella y los peces." alt="image" onclick="mostrarFoto(this);" />
-                    </div>
-                    
-                    <div class="inner">
-                        <img src="/img/fude4.jpg" class="img-responsive"  alt="image" data-descripcion="Un ambiente de paz..."  onclick="mostrarFoto(this);"/>
-                        
-                    </div>
-                    
-                    
-                    <div class="inner">
-                        <img src="/img/fude6.jpg" class="img-responsive"  data-descripcion="Siéntete de la mano con la naturaleza" alt="image" onclick="mostrarFoto(this);" /> 
-                    </div>
-                    <div class="inner">
-                        <img src="/img/fude_banner.jpg" class="img-responsive"  alt="image" data-descripcion="Ún sitio siempre verde.." onclick="mostrarFoto(this);"/>
-                    </div>
-                    
-                    <div class="inner">
-                        <img src="/img/fude7.jpg" class="img-responsive"  alt="image"  data-descripcion="gola" onclick="mostrarFoto(this);"/>
-                    </div>
-                    
-                    <div class="inner">
-                            
-                        <img src="/img/fude9.jpg" class="img-responsive"  alt="image" data-descripcion="Disfrutra de la naturaleza con nosotros." onclick="mostrarFoto(this);" />
-                            
-                    </div>
-                        
-                    
+                    @endforeach
                 </div>
             
             </div>
